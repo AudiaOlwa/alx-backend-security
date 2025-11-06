88b38c04ebcee1f536f12f415c14dcf24aca9249
Audia Olwa <audiaolwa@gmail.com>
2025-11-06 09:28:58 +0100

    IP Blacklisting

--- ip_tracking/middleware.py
+++ ip_tracking/middleware.py
@@ -2,6 +2,10 @@ from .models import RequestLog
 from django.utils.deprecation import MiddlewareMixin
 from django.utils import timezone
 from ipware import get_client_ip   # utile pour récupérer l'IP proprement
+from django.http import HttpResponseForbidden
+from .models import BlockedIP
+from django.utils.timezone import now
+from django.db import connection
 
 class IPLoggingMiddleware(MiddlewareMixin):
     def process_request(self, request):
@@ -14,3 +18,25 @@ class IPLoggingMiddleware(MiddlewareMixin):
             timestamp=timezone.now(),
             path=request.path
         )
+
+
+class IPTrackingMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        # Get IP
+        client_ip, _ = get_client_ip(request)
+
+        # ✅ Block request if IP is blacklisted
+        if client_ip and BlockedIP.objects.filter(ip_address=client_ip).exists():
+            return HttpResponseForbidden("Access denied. Your IP is blocked.")
+
+        # ✅ Log request
+        RequestLog.objects.create(
+            ip_address=client_ip or "unknown",
+            timestamp=now(),
+            path=request.path
+        )
+
+        return self.get_response(request)
