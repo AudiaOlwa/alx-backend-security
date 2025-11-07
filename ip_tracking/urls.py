de11ff9956d0d46eaf1d1ffd73300f330b1c9196
Audia Olwa <audiaolwa@gmail.com>
2025-11-07 12:02:34 +0100

    Anomaly Detection

--- ip_tracking/urls.py
+++ ip_tracking/urls.py
@@ -4,6 +4,4 @@ from .views import login_view, public_view
 urlpatterns = [
     path('login/', login_view),
     path('public/', public_view),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0))
-
 ]
