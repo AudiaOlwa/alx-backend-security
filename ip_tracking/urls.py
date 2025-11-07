e43d1cf32b7176781b04ab9e6d6417a7319cb7df
Audia Olwa <audiaolwa@gmail.com>
2025-11-07 15:01:44 +0100

    Anomaly Detection

--- ip_tracking/urls.py
+++ ip_tracking/urls.py
@@ -1,7 +1,21 @@
 from django.urls import path
 from .views import login_view, public_view
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="ALX Backend Security API",
+      default_version='v1',
+      description="API documentation",
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('login/', login_view),
     path('public/', public_view),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
