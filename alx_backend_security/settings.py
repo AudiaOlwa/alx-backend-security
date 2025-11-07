e57eb7cd6e165cb44133c41c72c7490ef139a5d4
Audia Olwa <audiaolwa@gmail.com>
2025-11-07 14:10:34 +0100

    Anomaly Detection

--- alx_backend_security/settings.py
+++ alx_backend_security/settings.py
@@ -18,7 +18,6 @@ CELERY_RESULT_BACKEND = os.environ.get('CELERY_RESULT_BACKEND', 'rpc://')
 
 # Sécurité & host
 #SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'dev-secret')
-#ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -33,7 +32,8 @@ SECRET_KEY = 'django-insecure-3eci*g%2et=nvt@@&rhkrp349mu*gvfx&)k_lkrxkonk*m7d9-
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = ['*']  # Ou ton domaine Render
+#ALLOWED_HOSTS = ['*']  # Ou ton domaine Render
+ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
