6df8d7e0f5a2d773bca3ec69d21d7584a33cf569
Audia Olwa <audiaolwa@gmail.com>
2025-11-07 13:53:57 +0100

    Anomaly Detection

--- alx_backend_security/settings.py
+++ alx_backend_security/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = 'django-insecure-3eci*g%2et=nvt@@&rhkrp349mu*gvfx&)k_lkrxkonk*m7d9-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 ALLOWED_HOSTS = ['*']  # Ou ton domaine Render
-#ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1']
+
 
 # Application definition
 
